c87a06fe5114c6ad1f949a9ca8c1701d68be7c27
Moniroth Suon <monirothsuon@gmail.com>
2018-08-30 18:57:58 +0000

    addedf STATIC_ROOT for serving of static files purposes

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ku6g5)ebtiftpuk8^+uhcun9hvx!e5o8n@-tz!w_gow5&i)5&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["monirothsuon.com"]
 
